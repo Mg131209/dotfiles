fcbf50c5f0c95bd71bba8d6badac5408749845fa
Mg1312 <miogalli@icloud.com>
2026-02-26 21:04:22 +0100

    add nvim configs

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.api.nvim_set_keymap("i", "<CapsLock>", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true })
+
+-- Enable shift + arrows in insert mode to select
+vim.api.nvim_set_keymap("i", "<S-Left>", "<Esc>v<Left>a", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<S-Right>", "<Esc>v<Right>a", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<S-Up>", "<Esc>v<Up>a", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<S-Down>", "<Esc>v<Down>a", { noremap = true, silent = true })
