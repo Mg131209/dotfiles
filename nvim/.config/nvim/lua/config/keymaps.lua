d592975abd6dbaac0fef7d8ce78c934f626e9bfa
Mg131209 <miogalli@icloud.com>
2026-04-03 17:54:47 +0200

    some changes and lock file removed from nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "<CapsLock>", "<Esc>", { noremap = true, silent = true })
-vim.keymap.set("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true, desc = "Swithc to next open bufer" })
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true, desc = "Switch to last bufer" })
 
 -- Enable shift + arrows in insert mode to select
 vim.api.nvim_set_keymap("i", "<S-Left>", "<Esc>v<Left>a", { noremap = true, silent = true })
