fcd3dd9dc5d81233132ede193e7240df96a03576
Mg131209 <miogalli@icloud.com>
2026-04-10 15:27:35 +0200

    some more nvim stuff

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,40 +1,39 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-    automatic_enable = true
-  },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			automatic_enable = true,
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.lsp.config = vim.lsp.config or {}
-      vim.lsp.config.capabilities = capabilities
+			vim.lsp.config = vim.lsp.config or {}
+			vim.lsp.config.capabilities = capabilities
 
-
-       vim.diagnostic.config({
-        virtual_text = {
-          source = "if_many",
-          prefix = "●",
-          spacing = 4,
-        },
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-        severity_sort = true,
-      })
-     end
-  }
+			vim.diagnostic.config({
+				virtual_text = {
+					source = "if_many",
+					prefix = "●",
+					spacing = 4,
+				},
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -3,3 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
