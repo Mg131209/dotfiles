c3d58812c15b25549001b4517920420305401c63
Mg131209 <miogalli@icloud.com>
2026-04-25 15:41:14 +0200

    added some stuff

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 
@@ -7,9 +10,9 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.rustfmt,
+				require("none-ls.diagnostics.eslint_d"),
 			},
 		})
-
 	end,
 }
